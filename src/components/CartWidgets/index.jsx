a6855e33f40dd26852a291daa4d91ee9891feeb5
leogumi <111820724+leogumi@users.noreply.github.com>
2022-12-18 20:51:58 -0300

    se creo navbar con cartwisget

---
+++ src/components/CartWidgets/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { TiShoppingCart } from "react-icons/ti";
+
+function CartWidgets() {
+  return (
+    <>
+    <TiShoppingCart style={{ height: 50, width: 50 }} />
+  <span>
+    10
+  </span>
+  </>
+  )
+
+}
+
+export default CartWidgets
